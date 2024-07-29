98df1c41dcdadc13e242e96a0390a415bf3378d1
adam-09-06 <zakiawadallah.04@gmail.com>
2024-07-29 15:14:33 +0300

    map msh sh8al

--- main.go
+++ main.go
@@ -11,21 +11,18 @@ type Room struct {
 	Name  string
 	Jeran []*Room
 }
+
 type Farm struct {
 	Rooms  map[string]*Room
 	AntNum int
 	Start  Room
 	End    Room
 }
-type Ant struct{
-	
-}
 
 var (
 	farm        Farm
 	mwjoodStart bool
 	mwjoodEnd   bool
-
 )
 
 func main() {
@@ -42,6 +39,8 @@ func ParseFile(file string) {
 	split := strings.Split(string(open), "\n")
 	farm.AntNum, or = strconv.Atoi(split[0])
 	Err(or)
+
+	farm.Rooms = make(map[string]*Room)
 
 	for _, line := range split[1:] {
 		if line == "" {
@@ -61,14 +60,21 @@ func ParseFile(file string) {
 			farm.Start.Name = f[0]
 			fmt.Println(farm.Start.Name)
 			mwjoodStart = false
+			continue
 		}
 		if mwjoodEnd {
 			farm.End.Name = f[0]
 			fmt.Println(farm.End.Name)
 			mwjoodEnd = false
+			continue
 		}
 		if strings.Contains(line, "-") {
-			
+			split := strings.Split(line, "-")
+			from := split[0]
+			to := split[1]
+			mapmaker(from, to)
+			fmt.Print(from + " ")
+			fmt.Println(to)
 		}
 	}
 }
@@ -78,4 +84,19 @@ func Err(Error error) {
 		fmt.Println(Error)
 		os.Exit(0)
 	}
+}
+
+func mapmaker(from, to string) {
+	fromNode, fromExists := farm.Rooms[from]
+	if !fromExists {
+		fromNode = &Room{Name: from}
+		farm.Rooms[from] = fromNode
+	}
+	toNode, toExists := farm.Rooms[to]
+	if !toExists {
+		toNode = &Room{Name: to}
+		farm.Rooms[to] = toNode
+	}
+	fromNode.Jeran = append(fromNode.Jeran, toNode)
+	toNode.Jeran = append(toNode.Jeran, fromNode)
 }
